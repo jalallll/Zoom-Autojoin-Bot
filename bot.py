3dfbd9a224106aad151ae0d5725aa1fc72b25584
Jalal <jalalq11@gmail.com>
2020-10-28 20:04:48 -0400

    sweg

---
+++ bot.py
@@ -0,0 +1,59 @@
+import subprocess
+import pyautogui
+import time
+import pandas as pd
+import cv2
+from datetime import datetime
+from PIL import ImageGrab
+import numpy as np
+
+
+def start(id, key):
+    print(id)
+    subprocess.call('C:\\Users\\Jalal\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe')
+    join = None
+
+    while join is None:
+        join = pyautogui.locateCenterOnScreen(
+            'join.png', grayscale=True)
+        print(join)
+    print(join)
+    pyautogui.moveTo(join)
+    pyautogui.click()
+
+    time.sleep(1)
+    pyautogui.write(id)
+    confirm = pyautogui.locateCenterOnScreen("confirm.PNG", grayscale=True)
+    pyautogui.moveTo(confirm)
+    pyautogui.click()
+    time.sleep(2)
+    pyautogui.write(key)
+    agn = pyautogui.locateCenterOnScreen("agn.PNG", grayscale=True)
+    pyautogui.moveTo(agn)
+    pyautogui.click()
+
+    time.sleep(2)
+    wo = pyautogui.locateCenterOnScreen("wo.PNG", grayscale=True)
+    pyautogui.moveTo(wo)
+    pyautogui.click()
+    time.sleep(1)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    o = cv2.VideoWriter("lec.avi", fourcc, 30.0, (3840, 2160))
+
+    go = True
+    while go == True:
+        rec = ImageGrab.grab()
+        ar = np.array(rec)
+        f = cv2.cvtColor(ar, cv2.COLOR_BGR2RGB)
+        cv2.imshow("Screen", f)
+        o.write(f)
+
+        # if cv2.waitKey(1) == ord('x') or cv2.waitKey(1) == 27:
+        #   cv2.destroyAllWindows()
+        #  o.release()
+        # go = False
+        time.sleep(4)
+        cv2.destroyAllWindows()
+        o.release()
+        go = False
---
+++ main.py
@@ -0,0 +1,3 @@
+from bot import start
+
+start("76446784105", "99n0UM")
