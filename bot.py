9b7f87035a067acbce2a998519431627c604c5e3
Jalal <jalalq11@gmail.com>
2020-10-31 11:03:32 -0400

    csv support

--- bot.py
+++ bot.py
@@ -8,52 +8,67 @@ from PIL import ImageGrab
 import numpy as np
 
 
-def start(id, key):
-    print(id)
-    subprocess.call('C:\\Users\\Jalal\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe')
-    join = None
+class Bot:
 
-    while join is None:
-        join = pyautogui.locateCenterOnScreen(
-            'join.png', grayscale=True)
+    def start(self, id, key):
+        print(id)
+        subprocess.call(
+            'C:\\Users\\Jalal\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe')
+        join = None
+
+        while join is None:
+            join = pyautogui.locateCenterOnScreen(
+                'join.png', grayscale=True)
+            print(join)
         print(join)
-    print(join)
-    pyautogui.moveTo(join)
-    pyautogui.click()
+        pyautogui.moveTo(join)
+        pyautogui.click()
 
-    time.sleep(1)
-    pyautogui.write(id)
-    confirm = pyautogui.locateCenterOnScreen("confirm.PNG", grayscale=True)
-    pyautogui.moveTo(confirm)
-    pyautogui.click()
-    time.sleep(2)
-    pyautogui.write(key)
-    agn = pyautogui.locateCenterOnScreen("agn.PNG", grayscale=True)
-    pyautogui.moveTo(agn)
-    pyautogui.click()
+        time.sleep(1)
+        pyautogui.write(id)
+        confirm = pyautogui.locateCenterOnScreen("confirm.PNG", grayscale=True)
+        pyautogui.moveTo(confirm)
+        pyautogui.click()
+        time.sleep(2)
+        pyautogui.write(key)
+        agn = pyautogui.locateCenterOnScreen("agn.PNG", grayscale=True)
+        pyautogui.moveTo(agn)
+        pyautogui.click()
 
-    time.sleep(2)
-    wo = pyautogui.locateCenterOnScreen("wo.PNG", grayscale=True)
-    pyautogui.moveTo(wo)
-    pyautogui.click()
-    time.sleep(1)
+        time.sleep(2)
+        wo = pyautogui.locateCenterOnScreen("wo.PNG", grayscale=True)
+        pyautogui.moveTo(wo)
+        pyautogui.click()
+        time.sleep(1)
 
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    o = cv2.VideoWriter("lec.avi", fourcc, 30.0, (3840, 2160))
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        o = cv2.VideoWriter("lec.avi", fourcc, 30.0, (3840, 2160))
 
-    go = True
-    while go == True:
-        rec = ImageGrab.grab()
-        ar = np.array(rec)
-        f = cv2.cvtColor(ar, cv2.COLOR_BGR2RGB)
-        cv2.imshow("Screen", f)
-        o.write(f)
+        go = True
 
-        # if cv2.waitKey(1) == ord('x') or cv2.waitKey(1) == 27:
-        #   cv2.destroyAllWindows()
-        #  o.release()
-        # go = False
-        time.sleep(4)
-        cv2.destroyAllWindows()
-        o.release()
-        go = False
+        while go == True:
+            rec = ImageGrab.grab()
+            ar = np.array(rec)
+            f = cv2.cvtColor(ar, cv2.COLOR_BGR2RGB)
+            cv2.imshow("Screen", f)
+            o.write(f)
+
+            if cv2.waitKey(1) == ord('x') or cv2.waitKey(1) == 27:
+                leave = pyautogui.locateCenterOnScreen(
+                    'leave.PNG', grayscale=False)
+                pyautogui.moveTo(leave)
+                pyautogui.click()
+                cv2.destroyAllWindows()
+                o.release()
+                Bot.terminate()
+                go = False
+        Bot.terminate()
+        return o, go
+
+    def terminate(self, msg):
+        print("Terminating")
+        print(msg)
+        leave = pyautogui.locateCenterOnScreen('leave.PNG', grayscale=False)
+        pyautogui.moveTo(leave)
+        pyautogui.click()
+        return "done"
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from bot import start
+from bot import Bot
+import pandas as pd
+import csv
+from datetime import datetime
 
-start("76446784105", "99n0UM")
+bot = Bot()
+bot2 = Bot()
+
+with open('file.csv') as csvfile:
+    f = csv.reader(csvfile, delimiter=',')
+    for i in f:
+        starting = i[0]
+        id = i[1]
+        key = i[2]
+time = datetime.now().strftime("%H:%M")
+
+t = "9:30"
+
+if starting == t:
+    print("starting")
+    o, go = bot.start(id, key)
+    done = bot2.terminate("noo")
